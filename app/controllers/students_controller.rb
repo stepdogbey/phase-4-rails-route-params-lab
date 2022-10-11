ac118f246c15eebec574b79868b7b261839fb5e4
stephen <stevedogbey@gmail.com>
2022-10-11 06:56:38 +0000

    params lab completd

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,11 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+    
+  end
+  def show
+    student_data = Student.find(params[:id])
+    render json: student_data
   end
 
 end
